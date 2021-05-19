280f47c9a8fc22db50a74f3f1311b45c14316658
diogodlemos <diogodrummond@hotmail.com>
2021-05-19 19:46:32 -0300

    Exercicio do dia 4.1 - 6 Exibe os movimentos da peça de Xadrez contida na variavel

--- bloco_4/dia_1/script.js
+++ bloco_4/dia_1/script.js
@@ -1,15 +1,27 @@
-let a1 = 45;
-let a2 = 45;
-let a3 = 90;
-let somaDosAngulos;
+let pecaDeXadrez = 'Rei';
 
-somaDosAngulos = a1 + a2 + a3;
+pecaDeXadrez = pecaDeXadrez.toLowerCase();
 
-if (somaDosAngulos === 180) {
-    console.log(true);
-}else if(a1 <= 0 || a2 <= 0 || a3 <= 0){
-    console.log('Erro!');
-}
-else {
-    console.log(false);
+switch (pecaDeXadrez) {
+    case 'rei':
+        console.log('O rei pode mover-se em todas as direções (horizontal, vertical e diagonal) somente uma casa de cada vez.');
+        break;
+    case 'rainha':
+        console.log('A rainha move-se ao longo da horizontal, vertical e diagonais mas não pode pular outras peças.');
+        break;  
+    case 'bispo':
+        console.log('O bispo move-se ao longo da diagonal. Não pode pular outras peças.');
+        break;
+    case 'cavalo':
+        console.log('É a única peça que pode pular as outras. O movimento do cavalo é em forma de “L”, quer dizer, duas casas em sentido horizontal e mais uma na vertical ou vice-versa.');
+        break;
+    case 'torre':
+        console.log('A torre movimenta-se pela vertical ou horizontal, mas não pode pular outras peças.');
+        break;  
+    case 'peao':
+        console.log('O peão movimenta-se apenas uma casa para frente e somente captura outras peças na diagonal. Opcionalmente, cada peão pode avançar duas casas no seu primeiro movimento do jogo.');
+        break;
+    default: {
+        console.log('Essa peça não existe no jogo de Xadrez');
+    }             
 }
