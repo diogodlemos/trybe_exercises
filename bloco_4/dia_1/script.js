eed49354b4fbe1c88bca6620ad06ad7f07984873
diogodlemos <diogodrummond@hotmail.com>
2021-05-19 19:15:07 -0300

    Exercicio do dia 4.1 - 4 Verifica se o número é positivo , negativo ou zero

--- bloco_4/dia_1/script.js
+++ bloco_4/dia_1/script.js
@@ -1,11 +1,9 @@
-let num1 = 4;
-let num2 = 8;
-let num3 = 23;
+let num = 10;
 
-if (num1 >= num2 && num1 >= num3 ) {
-    console.log('Este número é o maior : ' + num1);
-}else if (num2 >= num1 && num2 >= num3) {
-    console.log('Este número é o maior : ' + num2);
-}else if (num3 >= num1 && num3 >= num2) {
-    console.log('Este número é o maior : ' + num3);
+if (num > 0) {
+    console.log('Positive');
+}else if (num < 0) {
+    console.log('Negative');
+}else {
+    console.log('Zero');
 }
