a69438c7360e7dcc283ea387c1bd5865ec36f942
diogodlemos <diogodrummond@hotmail.com>
2021-05-20 19:06:45 -0300

    Exercicio Bônus do dia 4.2 - Codigo para ordernar em ordem crescente um array

--- bloco_4/dia_2/script.js
+++ bloco_4/dia_2/script.js
@@ -1,11 +1,12 @@
-let tamanho = 25;
-let array = [];
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for (let index = 1; index < (tamanho + 1); index += 1) {
-    array.push(index);
-}
-let arrayDividido = [];
-for (let index = 0; index < array.length; index += 1) {
-    arrayDividido.push(array[index]/2);
-}
-console.log(arrayDividido);
+for (let index = 1; index < numbers.length; index += 1) {
+    for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+        if (numbers[secondIndex] >  numbers[index]) {
+            let position = numbers[secondIndex];
+            numbers[secondIndex] = numbers[index];
+            numbers[index] = position;
+        }
+    }
+  }
+  console.log(numbers);
