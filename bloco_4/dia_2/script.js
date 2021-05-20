ca157672986dda73ebc5a7e8dcd43abe5bc1adef
diogodlemos <diogodrummond@hotmail.com>
2021-05-20 18:37:21 -0300

    Exercicio do dia 4.2 - 8 Cria um array de 1 a 25 utilizando for

--- bloco_4/dia_2/script.js
+++ bloco_4/dia_2/script.js
@@ -1,13 +1,7 @@
-let numbers = [5, 9, 3, 191, 70, 8, 100,2, 35, 27];
+let tamanho = 25;
+let array = [];
 
-let valoresImpares = 0;
-for (index = 0; index < numbers.length; index += 1) {
-    if(numbers[index] % 2 !== 0) {
-        valoresImpares += 1;
-    }
+for (let index = 1; index < (tamanho + 1); index += 1) {
+    array.push(index);
 }
-if(valoresImpares === 0){
-    console.log('Nenhum valor impar encontrado');
-}
-
-console.log('O numero de valores impares do Array é : '+valoresImpares);
+console.log(array);
