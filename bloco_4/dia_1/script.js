5ae3f01305a6f770489d749bf07bca4364283dff
diogodlemos <diogodrummond@hotmail.com>
2021-05-19 23:22:22 -0300

    Exercicio do dia 4.1 - 7 Exibe o Conceito de uma nota

--- bloco_4/dia_1/script.js
+++ bloco_4/dia_1/script.js
@@ -1,27 +1,15 @@
-let pecaDeXadrez = 'Rei';
+let nota = -10;
 
-pecaDeXadrez = pecaDeXadrez.toLowerCase();
-
-switch (pecaDeXadrez) {
-    case 'rei':
-        console.log('O rei pode mover-se em todas as direções (horizontal, vertical e diagonal) somente uma casa de cada vez.');
-        break;
-    case 'rainha':
-        console.log('A rainha move-se ao longo da horizontal, vertical e diagonais mas não pode pular outras peças.');
-        break;  
-    case 'bispo':
-        console.log('O bispo move-se ao longo da diagonal. Não pode pular outras peças.');
-        break;
-    case 'cavalo':
-        console.log('É a única peça que pode pular as outras. O movimento do cavalo é em forma de “L”, quer dizer, duas casas em sentido horizontal e mais uma na vertical ou vice-versa.');
-        break;
-    case 'torre':
-        console.log('A torre movimenta-se pela vertical ou horizontal, mas não pode pular outras peças.');
-        break;  
-    case 'peao':
-        console.log('O peão movimenta-se apenas uma casa para frente e somente captura outras peças na diagonal. Opcionalmente, cada peão pode avançar duas casas no seu primeiro movimento do jogo.');
-        break;
-    default: {
-        console.log('Essa peça não existe no jogo de Xadrez');
-    }             
+if (nota >= 90){
+    console.log('Conceito A');
+}else if (nota >= 80){
+    console.log('Conceito B');
+} else if (nota >= 70){
+    console.log('Conceito C');
+} else if (nota >= 60){
+    console.log('Conceito D');
+} else if (nota >= 50){
+    console.log('Conceito E');
+}else if (nota < 50) {
+    console.log('Conceito F');
 }
