3ef3284fdf733eb46876aeb82c27c5872709c487
diogodlemos <diogodrummond@hotmail.com>
2021-05-20 18:51:26 -0300

    Exercicio do dia 4.2 - 9 Imprime o resultado do vetor de 1 a 25 divido por 2

--- bloco_4/dia_2/script.js
+++ bloco_4/dia_2/script.js
@@ -4,4 +4,8 @@ let array = [];
 for (let index = 1; index < (tamanho + 1); index += 1) {
     array.push(index);
 }
-console.log(array);
+let arrayDividido = [];
+for (let index = 0; index < array.length; index += 1) {
+    arrayDividido.push(array[index]/2);
+}
+console.log(arrayDividido);
