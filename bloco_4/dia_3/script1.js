0537995f18a29676e1735d900c6756462974679c
diogodlemos <diogodrummond@hotmail.com>
2021-05-21 15:29:42 -0300

    Exercicio do dia 4.3 - 2 Imprime um triângulo retângulo  do tamanho da variavel n

--- bloco_4/dia_3/script1.js
+++ bloco_4/dia_3/script1.js
@@ -1,11 +1,12 @@
-let num = 10;
+let num = 15;
 
 let saida='';
 for (let coluna = 0; coluna < num; coluna += 1){
-    for (let linha = 1; linha <= num; linha += 1) {
+    for (let linha = 0; linha < num; linha += 1) {
         saida = saida + '*';
-        if(linha === num ) {
+        if(linha === coluna) {
             saida = saida + '\n';
+            linha = num;
         }
     }
 }
