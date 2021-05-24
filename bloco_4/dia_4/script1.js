d4b6e7a4a14b6d5d18ca95631314b68cbc32bde6
diogodlemos <diogodrummond@hotmail.com>
2021-05-24 19:17:41 -0300

    Exercicio do dia 4.4- 5 Imprimindo valores de dois objetos

--- bloco_4/dia_4/script1.js
+++ bloco_4/dia_4/script1.js
@@ -1,16 +1,16 @@
 let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  };
-
-  console.log('Boas Vindas '+info.personagem +' !');
-
-  info.recorrente = 'Sim';
-
-  console.log(info);
+      personagem: 'Margarida',
+      origem: 'Pato Donald',
+      nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+      recorrente: 'Sim'
+    };
+let segundaInfo = {
+      personagem: 'Tio Patinhas',
+      origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+      nota: 'O último MacPatinhas',
+      recorrente: 'Sim'
+    };
 
   for (let key in info) {
-    console.log(key);
-    console.log(info[key]);
+    console.log(info[key] + ' e '+segundaInfo[key]);
   }
