1c3a6d4632badea7b9637b5665eeec9fbeea996e
diogodlemos <diogodrummond@hotmail.com>
2021-06-21 18:58:32 -0300

    exercicios do dia 7.3 - numero 3

--- bloco_7/script.js
+++ bloco_7/script.js
@@ -1,19 +1,23 @@
 const assert = require('assert');
 
-function myRemove(arr, item) {
-  let newArr = [];
-  for (let index = 0; index < arr.length; index += 1) {
-    if (item !== arr[index]) {
-      newArr.push(arr[index]);
+function myRemoveWithoutCopy(arr, item) {
+  for (let index = 0, len = arr.length; index < len; index += 1) {
+    if (arr[index] === item) {
+      arr.splice(index, 1);
+      index -= 1;
+      len -= 1;
     }
   }
-  return newArr;
+
+  return arr;
 }
-// console.log(myRemove([1, 2, 3, 4], 3));
 
 // implemente seus testes aqui
-const array = [1, 2, 3, 4];
-const item = 5
-assert.deepStrictEqual( myRemove(array, item), [1, 2, 3, 4]);
-// assert.notDeepStrictEqual(myRemove(array, item), [1, 2, 3, 4]);
-// console.log(array);
+let array = [1, 2, 3, 4];
+const item = 5;
+assert.deepStrictEqual(myRemoveWithoutCopy(array, item), [1,2,3,4]);
+
+// assert.notDeepStrictEqual(myRemoveWithoutCopy(array, item), [1,2,3,4]);
+
+// myRemoveWithoutCopy(array, 3);
+// console.log(array); 
