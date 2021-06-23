f6bbc1cd5b1293a18d11abf55b54d6291fcde79c
diogodlemos <diogodrummond@hotmail.com>
2021-06-23 15:56:15 -0300

    Exercicio 3 do dia 8.1 - Confere array de respostas e retorna a pontuaçao

--- bloco_8/dia_1/script.js
+++ bloco_8/dia_1/script.js
@@ -1,13 +1,22 @@
-const checkDrawPrize = (num1) => {
-    const number = Math.ceil(Math.random()*5);
-    console.log(`sorteado: ${number}`);
-    console.log(`Apostado: ${num1}`);
-    if (num1 === number) return `Parabens vc ganhou!`;
-    return `Tente novamente!`;
+const rightAnswers = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const studentAnswers = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'C', 'B'];
+
+const checkAnswers = (answers,correctAnswers) => {
+  let points = 0;
+  for (let index = 0; index <= answers.length; index += 1){
+    if (answers[index] === correctAnswers[index]) {
+        points += 1;
+    } else if (answers[index] !== correctAnswers[index]) {
+        points -= 0.5;
+    } else if (answers[index] === 'N.A') {
+        points += 0;
+    }
+  }
+  return points;
 }
 
-const resultDrawPrize = (num, checkDrawPrize) => {
-  return checkDrawPrize(num);
+const verifyAnswers = (answers, correctAnswers, checkAnswers) => {
+  return checkAnswers(answers,correctAnswers);
 }
 
-console.log(resultDrawPrize(5,checkDrawPrize));
+console.log(verifyAnswers(studentAnswers,rightAnswers,checkAnswers));
