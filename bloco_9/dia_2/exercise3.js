6a6ad5a2758daa294409c81d9a7b2b72ca71f526
diogodlemos <diogodrummond@hotmail.com>
2021-07-07 21:17:20 -0300

    Exercicio 3 do dia 9.2

---
+++ bloco_9/dia_2/exercise3.js
@@ -0,0 +1,18 @@
+const myPromise = () => {
+    return new Promise ((resolve, reject) => {
+      const array = [];
+      for(let index = 0; index < 10; index += 1){
+        const number = Math.random() * 50 + 1;
+        array.push(number);
+      }
+      const sum = array.map((number)=>number*number)
+           .reduce((sum, number) => sum + number);
+      if (sum < 8000) {
+          resolve(sum);
+      }
+      reject();
+    })
+  }
+  
+  myPromise().then((number) => console.log(number , [number/2]))
+             .catch(()=> console.log('Promise rejeitada'));
