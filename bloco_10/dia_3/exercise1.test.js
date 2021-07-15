e2bc60ef14dde04a2e6a18d118cb4b963d603da1
diogodlemos <diogodrummond@hotmail.com>
2021-07-15 20:58:16 -0300

    Exercicios 2 e 3 do dia 10.3

--- bloco_10/dia_3/exercise1.test.js
+++ bloco_10/dia_3/exercise1.test.js
@@ -1,4 +1,7 @@
+const { generateRandomNumber } = require('./exercise1');
 const func = require('./exercise1');
+jest.mock('./exercise1');
+
 
 describe('When generateRandomNumber is called', () => {
   it('The function was called', () => {
@@ -7,5 +10,36 @@ describe('When generateRandomNumber is called', () => {
     expect(func.generateRandomNumber).toHaveBeenCalled();
     expect(func.generateRandomNumber()).toBe(10);
     expect(func.generateRandomNumber).toHaveBeenCalledTimes(2);
+  })
+
+  it('When create a new implemetation of operator division with parameters 6 e 2 return', () => {    
+    func.generateRandomNumber.mockImplementation((first, second) => first/second);
+    func.generateRandomNumber(6,2);
+
+    expect(func.generateRandomNumber(6,2)).toBe(3);
+    expect(func.generateRandomNumber).toHaveBeenCalled();
+    expect(func.generateRandomNumber).toHaveBeenCalledTimes(4);
+    expect(func.generateRandomNumber).toHaveBeenCalledWith(6,2);
+
+  })
+
+  it('When create a new implemetation of operator multiplication with parameters 4, 3 and 2 return, After reset and implement a new logic', () => {    
+    func.generateRandomNumber.mockImplementation((first, second, third) => first* second* third);
+    func.generateRandomNumber(4, 3, 2);
+
+    expect(func.generateRandomNumber(4, 3, 2)).toBe(24);
+    expect(func.generateRandomNumber).toHaveBeenCalled();
+    expect(func.generateRandomNumber).toHaveBeenCalledTimes(6);
+    expect(func.generateRandomNumber).toHaveBeenCalledWith(4, 3, 2);
+
+    func.generateRandomNumber
+      .mockReset()
+      .mockImplementation((number) => number * 2);
+    func.generateRandomNumber(3);
+
+    expect(func.generateRandomNumber(3)).toBe(6);
+    expect(func.generateRandomNumber).toHaveBeenCalled();
+    expect(func.generateRandomNumber).toHaveBeenCalledTimes(2);
+    expect(func.generateRandomNumber).toHaveBeenCalledWith(3);
   })
 })
